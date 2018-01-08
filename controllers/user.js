dae9005cf13f48fc7684ed17b88b632af839d74e
Alex Brooks <alexbrooks96@hotmail.co.uk>
2018-01-08 21:39:13 +0000

    initial commit

---
+++ controllers/user.js
@@ -0,0 +1,182 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+
+var User = require('../models/user');
+
+router.get('/', function(req, res){
+	res.render('index');
+});
+
+router.get('/signup', function (req, res){
+	msgError = req.flash('msgError')
+	res.render('signup', {message: msgError });
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+	successRedirect: '/profile',
+	failureRedirect: '/signup',
+	failureFlash: true
+}));
+
+router.get('/login', function (req, res){
+	login_error = req.flash('loginError')
+	password_error = req.flash('passwordError')
+	res.render('login', {loginError: login_error, passwordError: password_error});
+});
+
+router.post('/login', passport.authenticate('local.login', {
+	successRedirect: '/profile',
+	failureRedirect: '/login',
+	failureFlash: true
+}));
+
+
+router.get('/profile', isLoggedIn, function (req, res){
+	console.log(req.user);
+	res.render('profile', {user: req.user});
+});
+
+router.get('/logout', function (req, res){
+	req.logout();
+	res.redirect('/');
+});
+
+router.get('/newuser', function (req, res){
+	res.redirect('/signup');
+});
+
+router.get('/exisitingUser', function (req, res){
+	res.redirect('/login');
+});
+
+router.get('/increase', function (req, res, next){
+	console.log(req.user);
+	res.render('increase', {user: req.user});
+});
+
+router.post('/increaseBal', function (req, res, next){
+	var _id = req.body._id;
+	//var oldBal;
+	var newBal = parseFloat(req.body.value2);
+
+	newBal = newBal + req.user.balance;
+
+
+	var historyEntry = {
+		currBal : newBal,
+		timestamp : new Date(),
+	}
+
+	User.updateOne(
+		{
+			"_id": _id 
+		}, 
+		{
+			$set: {'balance': newBal},
+			$push: {'history': historyEntry}
+		},
+		function(err, result){
+		console.log('balance updated');
+
+		if(err) {
+			console.error(err);
+		}
+	});
+});
+
+
+router.get('/decrease', function (req, res, next){
+	console.log(req.user);
+	res.render('decrease', {user: req.user});
+});
+
+router.post('/decreaseBal', function (req, res, next){
+	var _id = req.body._id;
+	//var oldBal;
+	var newBal = parseFloat(req.body.value2);
+
+
+	newBal = req.user.balance - newBal;
+
+
+	var historyEntry = {
+		currBal : newBal,
+		timestamp : new Date(),
+	}
+
+	User.updateOne(
+		{
+			"_id": _id 
+		}, 
+		{
+			$set: {'balance': newBal},
+			$push: {'history': historyEntry}
+		},
+/*		{
+			$push: {<'history'>: 'currBal': newBal, 'timestamp': currDate},
+			//$push:{'history': newBal, currDate},
+		},
+*/
+		function(err, result){
+		console.log('balance updated');
+
+		if(err) {
+			console.error(err);
+		}
+	});
+});
+
+router.get('/edit', function (req, res, next){
+	console.log(req.user);
+	res.render('edit', {user: req.user});
+});
+
+router.post('/editUser', function (req, res, next){
+	var _id = req.body._id;
+	//var oldBal;
+	//var newBal = parseFloat(req.body.value2);
+
+	var newTarget = parseFloat(req.body.target);
+	var newFullName = String(req.body.fullname);
+	var newEmail = String(req.body.email);
+
+	//newBal = req.user.balance - newBal;
+
+
+	User.updateOne(
+		{
+			"_id": _id 
+		}, 
+		{
+			$set: {'target': newTarget, 'fullname': newFullName, 'email': newEmail}
+		},
+/*		{
+			$push: {'history': newBal, timestamp}
+		},*/
+
+		function(err, result){
+		console.log('profile updated');
+
+		if(err) {
+			console.error(err);
+		}
+	});
+});
+
+router.get('/history', function (req, res, next){
+	console.log(req.user);
+	res.render('history', {user: req.user});
+});
+
+
+module.exports = router;
+
+function isLoggedIn(req, res, next){
+	if (req.isAuthenticated()) {
+		return next();
+	}
+
+	res.redirect('/login');
+};
---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var mongoStore = require('connect-mongo')(session);
+var flash = require('express-flash');
+
+
+var app = express();
+
+
+mongoose.connect('mongodb://localhost/mongofm');
+
+var user = require('./controllers/user');
+
+require('./config/passport');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use(session({
+	secret:'mysecretsessionkey',
+	resave: true,
+	saveUninitialized: true,
+	store: new mongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.use(flash());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(user);
+
+
+
+app.listen(3000, function(){
+	console.log('listening on port 3000');
+});
